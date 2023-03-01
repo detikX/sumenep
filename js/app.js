db7e06d4b4db14e7052ddc4dfa09c6e3825561d1
dediwibisono17 <dedi.wibisono17@gmail.com>
2023-03-01 13:04:38 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,54 +1,54 @@
-$(document).ready(function(){
-    new WOW().init();
+$(document).ready(function () {
+  new WOW().init();
 })
 
 $('.all-photo').slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    autoplay: true,
-    slidesToShow: 2.1,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
+  dots: true,
+  infinite: true,
+  speed: 300,
+  autoplay: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
-    ]
-  });
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
-  //fancy box
-  $('[data-fancybox="gallery"]').fancybox({
-    buttons: [
-      "slideShow",
-      "thumbs",
-      "zoom",
-      "fullScreen",
-      "share",
-      "close"
-    ],
-    loop: false,
-    protect: true
-  });
+//fancy box
+$('[data-fancybox="gallery"]').fancybox({
+  buttons: [
+    "slideShow",
+    "thumbs",
+    "zoom",
+    "fullScreen",
+    "share",
+    "close"
+  ],
+  loop: false,
+  protect: true
+});
